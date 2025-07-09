7cb523567c79f26fcb9b5af63c726870acaac2e6
Seppe Mestdagh <mestdaghseppe@gmail.com>
2025-07-09 21:02:29 +0200

    add MVVM structure and Player model

---
+++ PGC/PGC/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGC.Models
+{
+    class Player
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Score { get; set; }
+
+        //Todo: images?
+
+    }
+}
---
+++ PGC/PGC/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PGC.Models;
+using System.Collections.ObjectModel;
+
+namespace PGC.ViewModels
+{
+    partial class MainViewModel : ObservableObject
+    {
+        // Players
+        [ObservableProperty]
+        private ObservableCollection<Player> _players;
+
+        [ObservableProperty]
+        private string _newPlayerName;
+
+        //// Sorted view of players
+        //[ObservableProperty]
+        //private ObservableCollection<Player> _sortedPlayers;
+
+        // Constructor additions
+        public MainViewModel()
+        {
+            Players = new ObservableCollection<Player>();
+        }
+
+        [RelayCommand]
+        void AddPlayer()
+        {
+            if (string.IsNullOrWhiteSpace(NewPlayerName))
+                return;
+
+            var player = new Player
+            {
+                Name = NewPlayerName,
+                Score = 0 // default score
+            };
+
+            if (!Players.Contains(player))
+                Players.Add(player);
+        }
+
+        [RelayCommand]
+        void RemovePlayer(Player player)
+        {
+            if (Players.Contains(player))
+                Players.Remove(player);
+        }
+
+    }
+}
